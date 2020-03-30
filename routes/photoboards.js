5b0404bf9b9854105f7b75625866a708f004b210
Spencer <zqian0336@gmail.com>
2020-03-30 09:43:41 -0700

    test

--- routes/photoboards.js
+++ routes/photoboards.js
@@ -41,8 +41,10 @@ var upload = multer({ storage: storage, fileFilter: imageFilter});
 
 cloudinary.config({
     cloud_name: 'heycameracloud',
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    // api_key: process.env.CLOUDINARY_API_KEY,
+    // api_secret: process.env.CLOUDINARY_API_SECRET
+    api_key: "376791236774896",
+    api_secret: "9FMfW8bOifudt50zYXxcDsVE_EQ"
 });
 
 
